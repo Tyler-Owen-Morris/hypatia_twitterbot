d1771d9a7455138658366aadea8588cdb4fdbc04
flicknewb <theowenmorris@gmail.com>
2023-04-30 18:06:08 -0700

    added reply capabilities, and subject history both being written to disk

--- src/bot.py
+++ src/bot.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import twitter_credentials as tc
 from time import sleep
+from datetime import datetime
 
 
 envpath = Path('.') / '.env'
@@ -20,32 +21,84 @@ openai.api_key = tc.OPENAI_KEY
 auth = OAuthHandler(tc.CONSUMER_KEY, tc.CONSUMER_SECRET)
 auth.set_access_token(tc.ACCESS_TOKEN, tc.ACCESS_TOKEN_SECRET)
 api = API(auth, wait_on_rate_limit=True)
-last_sub = 'Liverpool'
+last_sub = ''
 
 
 def run_bot():
+    client_info = api.verify_credentials()
+    client_id = client_info.__getattribute__('id')
+    print("my ID is:", client_id)
     while True:
-        # Query Twitter for trending topics
-        trending = get_trending()
-        print(trending)
-        # Determine if a tweet can be made
-        determined = determine_tweetability(trending)
-        print(determined)
-        # If so, generate a tweet on the corresponding topic, and make that tweet
-        det = determined[:3]
-        tweet = determined[3:]
-        subject = determine_subject(trending, tweet)
-        print("subject:", subject)
-        global last_sub
-        if last_sub != subject:
-            print(det)
-            last_sub = subject
-            if det == 'YES':
-                print("tweet this:", tweet)
-                api.update_status(tweet)
+        try:
+            reply_to_mentions(client_info)
+            # Query Twitter for trending topics
+            trending = get_trending()
+            # print(trending)
+            # Determine if a tweet can be made
+            determined = determine_tweetability(trending)
+            # print(determined)
+            # If so, generate a tweet on the corresponding topic, and make that tweet
+            det = determined.split(" - ")[0]
+            subj_str = determined.split(" - ")[-1]
+            subject = determine_subject(trending, subj_str)
+            print("determined:", det, " | subject:", subject)
+            if subject != None and det == 'YES':
+                eligible = determine_subject_eligibility(subject)
+                if eligible:
+                    tweet = make_tweet(subject)
+                    print("tweet this:", tweet)
+                    api.update_status(tweet)
+                else:
+                    print("tweeted about this subject too recently")
+            else:
+                print("Bot could not find a subject to Tweet about")
+            sleep(60*10)
+        except Exception as e:
+            print("error'd out:", e)
+            sleep(30)  # wait 30 seconds and try again
+            continue
+
+
+def reply_to_mentions(client_info):
+    # load info about who I am
+    my_id = client_info.__getattribute__('id')
+    # load historical conversations from disk
+    response = api.mentions_timeline(count=20)
+    for tweet in response:
+        # we do this each loop so we can write at the end of the loop
+        data = load_mentions_history()
+        # Load data about this tweet from the Status object in the array returned
+        tid = tweet.__getattribute__('id')
+        ttext = tweet.__getattribute__('text')
+        reply_user = tweet.__getattribute__('in_reply_to_user_id')
+        send_user = tweet.__getattribute__('user')
+        send_screenname = send_user.__getattribute__('screen_name')
+        print(send_screenname)
+        #print("mention:", ttext)
+        at_person = "@"+send_screenname+" "
+        if str(tid) not in data:
+            # reply to the person
+            reply = make_reply_tweet(ttext)  # "@"+send_screenname+" "
+            print("reply tweet:", reply)
+            if len(reply) > (280 - len(at_person)):
+                replies = split_tweet(reply, max_length=(280 - len(at_person)))
+                last_id = None
+                for repl in replies:
+                    if last_id == None:
+                        status = api.update_status(
+                            at_person+repl, in_reply_to_status_id=tid)
+                        last_id = status.__getattribute__('id')
+                    else:
+                        status = api.update_status(
+                            at_person+repl, in_reply_to_status_id=last_id)
+                        last_id = status.__getattribute__('id')
+            else:
+                api.update_status(reply, in_reply_to_status_id=tid)
+            data[tid] = [{"them": ttext, 'us': reply}]
         else:
-            print("already tweeted about", subject)
-        sleep(60*10)  # 60 seconds times n minuts
+            print("we have already replied to this according to history")
+        # save the conversation
+        write_mentions_history(data)
 
 
 def get_trending():
@@ -59,7 +112,7 @@ def get_trending():
 def determine_tweetability(topics):
     completion = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
-        messages=[{"role": "system", "content": "You are a classification bot. You will take in a comma separated list of trending twitter topics, and determine if you can make a tweet about any of those subjects and relate the subject to web3. If you can make the tweet reply 'YES' followed by the subject you have selected. If you cannot make the tweet, you will reply with 'NO'. All responses will start with 'YES' or 'NO '. You will only answer 'YES' or 'NO ' - you will not categorize all of the subjects. Reply 'OK' if you understand."},
+        messages=[{"role": "system", "content": "You are a classification bot. You will take in a comma separated list of trending twitter topics, and determine if you can make a tweet about any of those subjects and relate the subject to web3. If you can make the tweet reply 'YES' followed by the subject you have selected separated by the strin ' - '. If you cannot make the tweet, you will reply ONLY with the string 'NO '. All responses must start with the characters 'YES' or 'NO '. You will only answer 'YES' followed by ' - ' and the subject, or 'NO ' - you will not categorize all of the subjects. Reply 'OK' if you understand."},
                   {"role": "assistant", "content": "OK"},
                   {"role": "user", "content": ", ".join(topics)}]
     )
@@ -67,11 +120,116 @@ def determine_tweetability(topics):
     return resp
 
 
-def determine_subject(trending, tweet):
+def make_tweet(topic):
+    completion = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{"role": "system", "content": "You are a twitter bot. I will give you a popular subject, and you will generate a tweet relating that subject to web3 with the goal of generating as much traffic as possible. Ensure your tweets are within the character limit, and inspire users to reply. Reply with 'OK' if you understand"},
+                  {"role": "assistant", "content": "OK"},
+                  {"role": "user", "content": topic}]
+    )
+    resp = completion.choices[0].message.content
+    return resp
+
+
+def make_reply_tweet(topic):
+    completion = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{"role": "system", "content": "You are a twitter bot. I will give you the text of someone tweeting at you, and you will generate a reply. Your goal is to answer questions around web3 technologies, support community developers, and encourage new interest in web3. You will do your best to be nice to others, and carry on a respectful conversation. your character limit is 250 - you must never use more characters than 250. Reply with 'OK' if you understand"},
+                  {"role": "assistant", "content": "OK"},
+                  {"role": "user", "content": topic}]
+    )
+    resp = completion.choices[0].message.content
+    return resp
+
+
+def load_mentions_history():
+    file_name = "data/mentions.json"
+    # Write the empty file if it doesn't exist
+    if not os.path.exists(file_name):
+        with open(file_name, "w") as json_file:
+            json.dump({}, json_file)
+    # Read the file data
+    with open(file_name, "r") as json_file:
+        data = json.load(json_file)
+    # returns a dictionary of the historical tweets
+    return data
+
+
+def write_mentions_history(data):
+    file_name = "data/mentions.json"
+    with open(file_name, "w") as json_file:
+        json.dump(data, json_file)
+
+
+def load_subject_history():
+    file_name = f"data/history.json"
+    # Write the empty file if it doesn't exist
+    if not os.path.exists(file_name):
+        with open(file_name, "w") as json_file:
+            json.dump({}, json_file)
+    # Read the file data
+    with open(file_name, "r") as json_file:
+        data = json.load(json_file)
+    # returns a dictionary of the historical tweets
+    return data
+
+
+def determine_subject(trending, subj_str):
     for trend in trending:
-        if trend in tweet:
+        if trend in subj_str:
             return trend
     return None
+
+
+def split_tweet(text, max_length=280):
+    words = text.split()
+    tweets = []
+    current_tweet = ""
+
+    for word in words:
+        # If adding the word to the current tweet would exceed the max_length
+        if len(current_tweet) + len(word) + 1 > max_length:
+            # If the current tweet is not empty, add it to the tweets list
+            if current_tweet:
+                tweets.append(current_tweet.strip())
+
+            # Start a new tweet with the current word
+            current_tweet = word
+        else:
+            # Otherwise, add the word to the current tweet
+            current_tweet += " " + word
+
+    # Add the last tweet to the tweets list, if not empty
+    if current_tweet:
+        tweets.append(current_tweet.strip())
+
+    return tweets
+
+
+def determine_subject_eligibility(subj):
+    min_between_subjet_repeats = 30
+    file_name = f"data/history.json"
+    utc_now = datetime.utcnow()
+    ret = False
+    with open(file_name, "r") as json_file:
+        data = json.load(json_file)
+    if subj in data:
+        # history exists
+        last_time = datetime.strptime(data[subj], "%Y-%m-%d %H:%M:%S")
+        timediff = utc_now - last_time
+        minutes = timediff.total_seconds() / 60
+        if minutes > min_between_subjet_repeats:
+            ret = True
+            data[subj] = utc_now.strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            ret = False
+    else:
+        data[subj] = utc_now.strftime("%Y-%m-%d %H:%M:%S")
+        ret = True
+    # Write the updated data object back to disk
+    with open(file_name, "w") as json_file:
+        json.dump(data, json_file)
+    return ret
 
 
 if __name__ == "__main__":
