a0283b69c58486982121ef03569d7fda97ee6384
flicknewb <theowenmorris@gmail.com>
2023-05-05 14:55:24 -0700

    updating code for deployment

--- src/bot.py
+++ src/bot.py
@@ -7,8 +7,6 @@ from dotenv import load_dotenv
 import twitter_credentials as tc
 from time import sleep
 from datetime import datetime
-import random
-import pandas as pd
 
 
 envpath = Path('.') / '.env'
@@ -17,11 +15,11 @@ load_dotenv(dotenv_path=envpath)
 # setup the openapi auth
 openai.api_key = tc.OPENAI_KEY
 
-# auth = OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
-# auth.set_access_token(os.environ['ACCESS_TOKEN'],
-#                       os.environ['ACCESS_TOKEN_SECRET'])
-auth = OAuthHandler(tc.CONSUMER_KEY, tc.CONSUMER_SECRET)
-auth.set_access_token(tc.ACCESS_TOKEN, tc.ACCESS_TOKEN_SECRET)
+auth = OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN'],
+                      os.environ['ACCESS_TOKEN_SECRET'])
+# auth = OAuthHandler(tc.CONSUMER_KEY, tc.CONSUMER_SECRET)
+# auth.set_access_token(tc.ACCESS_TOKEN, tc.ACCESS_TOKEN_SECRET)
 api = API(auth, wait_on_rate_limit=True)
 
 
@@ -46,8 +44,13 @@ def reply_to_mentions(client_info):
     # load historical conversations from disk
     data_ref = load_primed_data()
     # fetch reply history
-    response = api.mentions_timeline(count=100, tweet_mode="extended")
+    response = api.mentions_timeline(count=200, tweet_mode="extended")
+    # Read the file data
+    # file_name = "data/sample_inputs.json"
+    # with open(file_name, "r") as json_file:
+    #     response = json.load(json_file)
     for tweet in response:
+        # print(tweet, type(tweet))
         # we do this each loop so we can write at the end of the loop
         data = load_mentions_history()
         # Load data about this tweet from the Status object in the array returned
@@ -56,34 +59,45 @@ def reply_to_mentions(client_info):
         reply_user = tweet.__getattribute__('in_reply_to_user_id')
         send_user = tweet.__getattribute__('user')
         send_screenname = send_user.__getattribute__('screen_name')
-        print(send_screenname)
+        # tid = tweet['id']
+        # ttext = tweet['full_text']
+        # send_user = tweet['user']
+        # send_screenname = tweet['screen_name']
+        # print(send_screenname)
         print("mention:", ttext)
         at_person = "@"+send_screenname+" "
         if str(tid) not in data:
             # determine subject of tweet
+            for s in data_ref.keys():
+                print(s)
             subj = determine_tweet_subject(ttext, list(data_ref.keys()))
             mysubj = determine_subject(subj)
             # reply to the person
-            reply = make_reply_tweet(ttext, mysubj)  # "@"+send_screenname+" "
-            print("reply tweet:", reply)
-            # if len(reply) > (280 - len(at_person)):
-            #     replies = split_tweet(reply, max_length=(280 - len(at_person)))
-            #     last_id = None
-            #     for repl in replies:
-            #         if last_id == None:
-            #             status = api.update_status(
-            #                 at_person+repl, in_reply_to_status_id=tid)
-            #             last_id = status.__getattribute__('id')
-            #         else:
-            #             status = api.update_status(
-            #                 at_person+repl, in_reply_to_status_id=last_id)
-            #             last_id = status.__getattribute__('id')
-            # else:
-            #     api.update_status(at_person+repl, in_reply_to_status_id=tid)
-            data[tid] = {"sender": send_screenname,
-                         "subject": mysubj, "tweet_text": ttext, 'reply': at_person+reply}
+            if mysubj == None:
+                data[tid] = {"sender": send_screenname,
+                             "subject": mysubj, "tweet_text": ttext, 'reply': "Not Replying."}
+            else:
+                # "@"+send_screenname+" "
+                reply = make_reply_tweet(ttext, mysubj)
+                print("reply tweet:", reply)
+                # if len(reply) > (280 - len(at_person)):
+                #     replies = split_tweet(reply, max_length=(280 - len(at_person)))
+                #     last_id = None
+                #     for repl in replies:
+                #         if last_id == None:
+                #             status = api.update_status(
+                #                 at_person+repl, in_reply_to_status_id=tid)
+                #             last_id = status.__getattribute__('id')
+                #         else:
+                #             status = api.update_status(
+                #                 at_person+repl, in_reply_to_status_id=last_id)
+                #             last_id = status.__getattribute__('id')
+                # else:
+                #     api.update_status(at_person+repl, in_reply_to_status_id=tid)
+                data[tid] = {"sender": send_screenname,
+                             "subject": mysubj, "tweet_text": ttext, 'reply': at_person+reply}
         else:
-            print("we have already replied to this according to history")
+            #print("we have already replied to this according to history")
             pass
         # save the conversation
         write_mentions_history(data)
@@ -92,7 +106,7 @@ def reply_to_mentions(client_info):
 def determine_subject(subj):
     loaded = load_primed_data()
     for subject in list(loaded.keys()):
-        if subj in subject:
+        if subj.lower() in subject.lower() or subject.lower() in subj.lower():
             return subject
     return None
 
@@ -104,7 +118,7 @@ def make_reply_tweet(tweet, subj):
         loaded_data = ""
     completion = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
-        messages=[{"role": "system", "content": "You are a twitter bot. You will be fed all the relevant data you need on a subject, and a user tweet. you will reply with the most informative and helpful response possible. reply with 'OK' if you understand."},
+        messages=[{"role": "system", "content": "You are a twitter bot. You will be fed all the relevant data you need on a subject, and a user tweet. you will reply with the most informative, helpful, and succinct response possible. reply with 'OK' if you understand."},
                   {"role": "assistant", "content": "OK"},
                   {"role": "system", "content": f"Data: {loaded_data}"},
                   {"role": "user", "content": tweet}]
@@ -136,7 +150,8 @@ def load_primed_data():
             data = json.load(json_file)
         # returns a dictionary of the historical tweets
         return data
-    except:
+    except Exception as e:
+        print("file-load failed - loading nothing", e)
         return {}
 
 
@@ -199,9 +214,10 @@ def determine_subject_eligibility(subj):
 
 def determine_tweet_subject(tweet, subjects):
     subjs = ",".join(subjects)
+    print("eligible subjects:", subjs)
     completion = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
-        messages=[{"role": "system", "content": f"You are a classification bot. The user will feed you a tweet and you will return which subject it belongs to with ONLY the name of the subject. The only eligible subjects are: {subjs}. you will not elaborate. you will not add extra words. You will JUST reply with the single subject. Reply with 'ok' if you understand."},
+        messages=[{"role": "system", "content": f"You are a classification bot. The user will feed you a tweet and you will return which subject it belongs to with ONLY the name of the subject. The only eligible subjects are: {subjs}. you will not elaborate. you will not add extra words. You will JUST reply with the single subject. The subject you reply with MUST be in the provided list: {subjs}. You will not invent new subjects- the subject will ONLY be one of these: {subjs}. If the tweet is not related to any of these subjects you will reply with the string 'None'. Reply with 'ok' if you understand."},
                   {"role": "assistant", "content": "OK"},
                   {"role": "user", "content": tweet}]
     )
